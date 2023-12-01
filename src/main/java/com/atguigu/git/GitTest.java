70367bab24056a049f482d46ee1a031271c969e4
aldwdsj <1904549735@qq.com>
2023-12-01 13:08:51 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("love");
+        System.out.println("push test");
     }
 }
